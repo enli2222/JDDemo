e67779b6b2b355b8cdbe6702df62112ed91507ab
Shaowu Cai@work <jiandandream@foxmail.com>
2015-10-16 16:16:22 +0800

    添加了将按钮转为竖直样式的方法

--- JDDemo/JDUtils/Category/UIButton+JDUtils.h
+++ JDDemo/JDUtils/Category/UIButton+JDUtils.h
@@ -18,4 +18,18 @@
 
 + (UIButton *)jd_buttonWithNormalTitle:(NSString *)title;
 
+
+//以下2个方法，须在设置图片和标题之后使用
+/**
+ *  将按钮转换为竖直样式：图片在上，标题在下
+ */
+- (void)jd_transToVertical;
+
+/**
+ *  将按钮转换为竖直样式：图片在上，标题在下
+ *
+ *  @param padding 图片与标题间距
+ */
+- (void)jd_transToVerticalWithPadding:(CGFloat)padding;
+
 @end
--- JDDemo/JDUtils/Category/UIButton+JDUtils.m
+++ JDDemo/JDUtils/Category/UIButton+JDUtils.m
@@ -37,5 +37,24 @@
     return button;
 }
 
+//来源于：http://my.oschina.net/sayonala/blog/198376
+- (void)jd_transToVerticalWithPadding:(CGFloat)padding {
+    // get the size of the elements here for readability
+    CGSize imageSize = self.imageView.frame.size;
+    CGSize titleSize = self.titleLabel.frame.size;
+    
+    // get the height they will take up as a unit
+    CGFloat totalHeight = (imageSize.height + titleSize.height + padding);
+    
+    // raise the image and push it right to center it
+    self.imageEdgeInsets = UIEdgeInsetsMake(- (totalHeight - imageSize.height), 0.0, 0.0, - titleSize.width);
+    
+    // lower the text and push it left to center it
+    self.titleEdgeInsets = UIEdgeInsetsMake(0.0, - imageSize.width, - (totalHeight - titleSize.height),0.0);
+}
+
+- (void)jd_transToVertical {
+    return [self jd_transToVerticalWithPadding:3.0f];
+}
 
 @end
