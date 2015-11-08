709da28de8391249216fa8104b48ce9a03073de0
Shaowu Cai <jiandandream@foxmail.com>
2015-11-08 21:45:36 +0800

    添加了UINavigationController+JDUtils

---
+++ JDDemo/JDUtils/Category/UINavigationController+JDUtils.h
@@ -0,0 +1,13 @@
+//
+//  UINavigationController+JDUtils.h
+//  JDDemo
+//
+//  Created by 蔡少武 on 15/11/8.
+//  Copyright © 2015年 jiandan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UINavigationController (JDUtils)
+
+@end
---
+++ JDDemo/JDUtils/Category/UINavigationController+JDUtils.m
@@ -0,0 +1,13 @@
+//
+//  UINavigationController+JDUtils.m
+//  JDDemo
+//
+//  Created by 蔡少武 on 15/11/8.
+//  Copyright © 2015年 jiandan. All rights reserved.
+//
+
+#import "UINavigationController+JDUtils.h"
+
+@implementation UINavigationController (JDUtils)
+
+@end
